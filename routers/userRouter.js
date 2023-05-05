fc0fa820667cccd7a30d65a031ccb0c781bad49d
mahmoudmohamedsh <201727100@std.sci.cu.edu.eg>
2023-05-05 15:02:50 +0300

    finish auth except img

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -4,6 +4,21 @@ const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const UserModel = require('../models/userModel');
 
+router.post('/signupVerify',[
+    check('email')
+    .notEmpty()
+    .withMessage('please enter a valid email.')
+    .custom((value,{req})=>{
+        return UserModel.findOne({email:value}).then(userDoc=>{
+                
+            if(userDoc){//user already exist 
+                console.log('eror')
+                return Promise.reject("email already exist");
+            }
+        })
+    }).normalizeEmail()
+    ],userController.signinVerify)
+
 router.post('/signup', [
     check('email').notEmpty().withMessage('please enter a valid email.')
     .custom((value,{req})=>{
@@ -17,8 +32,10 @@ router.post('/signup', [
     }).normalizeEmail()
     ,
     check('password').isLength({min:5}),
-    check('userName').not().isEmpty()
+    check('name').not().isEmpty()
 ], userController.signup);
+
+
 router.post('/signin', userController.signin);
 
 
